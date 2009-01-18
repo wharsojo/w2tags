5b077466737afb513f6032d8b23f5d21f07457a5
Widi Harsojo <wharsojo@gmail.com>
2009-01-19 01:16:46 +0700

    Update to latest rev

--- tasks/setup.rb
+++ tasks/setup.rb
@@ -17,7 +17,7 @@ PROJ = OpenStruct.new(
   :authors => nil,
   :email => nil,
   :url => "\000",
-  :version => ENV['VERSION'] || '0.8.5',
+  :version => ENV['VERSION'] || '0.8.6',
   :exclude => %w(tmp$ bak$ ~$ CVS \.svn/ \.git/ ^pkg/),
   :release_name => ENV['RELEASE'],
 
