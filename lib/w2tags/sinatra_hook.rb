94201cce07da8ee95acf68e2604ffbe2baf2d99d
widi <widi@ubuntu.ubuntu-domain>
2009-05-22 13:26:41 +0700

    Add Condition Hook for Sinatra 0.9.2

--- lib/w2tags/sinatra_hook.rb
+++ lib/w2tags/sinatra_hook.rb
@@ -31,14 +31,38 @@ module Sinatra
       
       module W2TagsHooked
         W2TAGS = W2Tags::Parser.new
-        def lookup_template(engine, template, options={})
-          case template
-          when Symbol
-            path= template_path(engine, template, options)
-            src = path.gsub(path[/(\.\w+)$/,1],'.w2'<<$1[1,9])
-            W2TAGS.parse_file(src,false,true)
+        if Sinatra::VERSION > '0.9.1'
+          def lookup_template(engine, template, views_dir, filename = nil, line = nil)
+            case template
+            when Symbol
+              if cached = self.class.templates[template]
+                lookup_template(engine, cached[:template], views_dir, cached[:filename], cached[:line])
+              else
+                path = ::File.join(views_dir, "#{template}.#{engine}")
+                src = path.gsub(path[/(\.\w+)$/,1],'.w2'<<$1[1,9])
+                W2TAGS.parse_file(src,false,true)
+                [ ::File.read(path), path, 1 ]
+              end
+            when Proc
+              filename, line = self.class.caller_locations.first if filename.nil?
+              [ template.call, filename, line.to_i ]
+            when String
+              filename, line = self.class.caller_locations.first if filename.nil?
+              [ template, filename, line.to_i ]
+            else
+              raise ArgumentError
+            end
           end
-          super
+        else
+          def lookup_template(engine, template, options={})
+            case template
+            when Symbol
+              path= template_path(engine, template, options)
+              src = path.gsub(path[/(\.\w+)$/,1],'.w2'<<$1[1,9])
+              W2TAGS.parse_file(src,false,true)
+            end
+            super
+          end
         end
       end
       
