e93fac501df338cd78159b9f1032c603326df426
Widi Harsojo <wharsojo@gmail.com>
2009-10-27 00:38:51 -0800

    Update Version

--- tasks/setup.rb
+++ tasks/setup.rb
@@ -17,7 +17,7 @@ PROJ = OpenStruct.new(
   :authors => 'Widi Harsojo',
   :email => 'wharsojo@gmail.com',
   :url => "\000",
-  :version => ENV['VERSION'] || '0.9.52',
+  :version => ENV['VERSION'] || '0.9.53',
   :exclude => %w(tmp$ bak$ ~$ CVS \.svn/ \.git/ ^pkg/),
   :release_name => ENV['RELEASE'],
 
